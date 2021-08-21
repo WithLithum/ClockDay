c4ca2cd7d2e56a91674d1c97b2d2d04a88ec7ace
RelaperCrystal <RelaperCrystal@163.com>
2021-08-21 09:22:03 +0800

    Slightly moved FPS counter position

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -21,7 +21,7 @@ namespace ClockDayRage
         public static void Main()
         {
             var _text = new ScaledText(new PointF(15, 5), "STARTING CLOCKDAY", 0.30f);
-            _fps = new ScaledText(new PointF(85, 5), "STARTING CLOCKDAY", 0.30f);
+            _fps = new ScaledText(new PointF(97, 5), "STARTING CLOCKDAY", 0.30f);
             var _background = new ScaledRectangle(new PointF(10, 5), new SizeF(85f + 65f + 25f, 25f));
             _background.Color = Color.FromArgb(150, 0, 0, 0);
             
